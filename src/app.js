c2f8f3122f8e3d2f7691a8e884d25de047ed8322
LuisGuerrero7 <158310573+LuisGuerrero7@users.noreply.github.com>
2024-06-16 15:33:07 +0000

    third project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomIndexExcuse(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  function generateExcuse() {
+    return (
+      "OMG you won't believe me but... " +
+      randomIndexExcuse(who) +
+      " " +
+      randomIndexExcuse(action) +
+      " " +
+      randomIndexExcuse(what) +
+      " " +
+      randomIndexExcuse(when)
+    );
+  }
+
+  const excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = generateExcuse();
 };
